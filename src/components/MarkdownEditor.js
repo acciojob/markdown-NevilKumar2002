b8b62d7e86c9b7585933dc536fe5774e7fd5fe5c
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-10-31 05:50:37 +0000

    First

---
+++ src/components/MarkdownEditor.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+const MarkdownEditor = () => {
+  const [markdown, setMarkdown] = useState('');
+  const [html, setHtml] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    // Convert the markdown text to HTML and update the 'html' state
+    const convertToHTML = () => {
+      // Implement the logic to convert markdown to HTML here
+      // You can use a library like 'marked' or 'remarkable' for this purpose
+      const convertedHtml = yourMarkdownToHtmlFunction(markdown);
+      setHtml(convertedHtml);
+    };
+
+    setIsLoading(true);
+    setTimeout(() => {
+      convertToHTML();
+      setIsLoading(false);
+    }, 1000); // Simulating a delay for loading
+  }, [markdown]);
+
+  const handleInputChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  return (
+    <div className="markdown-editor">
+      <div className="textarea">
+        <textarea
+          placeholder="Write your markdown here"
+          value={markdown}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="preview">
+        {isLoading ? <div className="loading">Loading...</div> : <div dangerouslySetInnerHTML={{ __html: html }} />}
+      </div>
+    </div>
+  );
+};
+
+export default MarkdownEditor;
